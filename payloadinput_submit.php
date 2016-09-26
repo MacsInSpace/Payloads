1f2ca3aff1a98a7fd7c274b7b05436df3cd5e125
Craig Hair <craig.hair@gmail.com>
2016-09-26 23:48:56 +1000

    Update payloadinput_submit.php
    Added "pretty" xml formatting from http://www.devnetwork.net/viewtopic.php?f=29&t=40331 and http://www.daveperrett.com/articles/2007/04/05/format-xml-with-php/ and stach overflow http://stackoverflow.com/questions/3616540/format-xml-string

--- payloadinput_submit.php
+++ payloadinput_submit.php
@@ -10,6 +10,51 @@ function checkInput($keys) {
 	}
 }
 
+function prettyXML($xml, $debug=false) {
+  //http://www.devnetwork.net/viewtopic.php?f=29&t=40331
+  //http://www.daveperrett.com/articles/2007/04/05/format-xml-with-php/
+  // add marker linefeeds to aid the pretty-tokeniser
+  // adds a linefeed between all tag-end boundaries
+  $xml = preg_replace('/(>)(<)(\/*)/', "$1\n$2$3", $xml);
+
+  // now pretty it up (indent the tags)
+  $tok = strtok($xml, "\n");
+  $formatted = ''; // holds pretty version as it is built
+  $pad = 4; // initial indent
+  $matches = array(); // returns from preg_matches()
+
+  /* pre- and post- adjustments to the padding indent are made, so changes can be applied to
+   * the current line or subsequent lines, or both
+  */
+  while($tok !== false) { // scan each line and adjust indent based on opening/closing tags
+
+    // test for the various tag states
+    if (preg_match('/.+<\/[^>]*>$/', $tok, $matches)) { // open and closing tags on same line
+      if($debug) echo " =$tok= ";
+      $indent = 0; // no change
+    }
+    else if (preg_match('/^<\//', $tok, $matches)) { // closing tag
+      if($debug) echo " -$tok- ";
+      $pad -= 4; //  outdent now
+    }
+    else if (preg_match('/^<[^>]*[^\/]>.*$/', $tok, $matches)) { // opening tag
+      if($debug) echo " +$tok+ ";
+      $indent = 4; // don't pad this one, only subsequent tags
+    }
+    else {
+      if($debug) echo " !$tok! ";
+      $indent = 0; // no indentation needed
+    }
+    
+    // pad the line with the required number of leading spaces
+    $prettyLine = str_pad($tok, strlen($tok)+$pad, ' ', STR_PAD_LEFT);
+    $formatted .= $prettyLine . "\n"; // add to the cumulative result, with linefeed
+    $tok = strtok("\n"); // get the next token
+    $pad += $indent; // update the pad size for subsequent lines
+  }
+  return $formatted; // pretty format
+}
+
 checkInput(['description', 'email', 'name', 'username']);
 $description = $_POST["description"];
 $email = $_POST["email"];
@@ -35,7 +80,6 @@ $xml = $payload->getXML()
 ?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
-<dict>
-<?=$xml?>
+<dict><?php echo "\n" . prettyXML($xml, false);?>
 </dict>
 </plist>
