c485c84bbe1e6b7111b2890d3bd8935e14dfd92b
Craig Hair <craig.hair@gmail.com>
2016-09-26 23:55:23 +1000

    Update payloadinput_submit.php

--- payloadinput_submit.php
+++ payloadinput_submit.php
@@ -78,6 +78,7 @@ $mail = new Mail($description, $fullname, $email, $username, $identifier, $serve
 $payload->addPayloadContent($mail->getXML());
 $xml = $payload->getXML()
 ?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
